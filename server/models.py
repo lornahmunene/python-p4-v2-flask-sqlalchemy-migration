ad1cbdb543da088f92dcf152d9ccfa84bab1f875
LornahMunene <munenelornah9@gmail.com>
2024-07-01 20:15:06 +0300

    migrations

--- server/models.py
+++ server/models.py
@@ -16,6 +16,16 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     salary = db.Column(db.Integer)
-
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    location = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name}, {self.location}>'
+
+    
